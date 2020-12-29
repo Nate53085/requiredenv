f913e034caac750766958c9321b03e9acfeac9ec
Nate Bourgoin <NateBourgoin@live.com>
2020-12-28 19:28:39 -0500

    Enable eslint and fix any detected issues

--- tests/tests.js
+++ tests/tests.js
@@ -7,17 +7,17 @@ tap.throws(function () {
   requiredEnv.checkRequired();
 }, new Error('ENOENT: no such file or directory, open \'required.env\''), 'An Error should be thrown if requirements file doesn\'t exist');
 
-tap.doesNotThrow('lines starting with # should be ignored as comments', test => {
+tap.doesNotThrow('lines starting with # should be ignored as comments', () => {
   process.env.BUT_DO_THIS = "value ignored";
   requiredEnv.checkRequired( { environmentDefinition : 'tests/test_files/ignore_comments.env' });
 });
 
-tap.doesNotThrow('basic single required.env parses without throwing', test => {
+tap.doesNotThrow('basic single required.env parses without throwing', () => {
   process.env.TEST_ENV_VAR = "test value";
   requiredEnv.checkRequired({ environmentDefinition : 'tests/test_files/single.env' });
 });
 
-tap.doesNotThrow('multiple entries parses without throwing', test => {
+tap.doesNotThrow('multiple entries parses without throwing', () => {
   process.env.TEST1 = "TEST1";
   process.env.TEST2 = "TEST2";
   process.env.TEST3 = "TEST3";
@@ -28,12 +28,12 @@ tap.throws(function () {
   requiredEnv.checkRequired({ environmentDefinition : 'tests/test_files/single_missing.env'});
 }, new Error('Missing environment variable MISSING'), 'An Error should be thrown with the missing environment variable');
 
-tap.doesNotThrow('plays well with dotenv', test => {
+tap.doesNotThrow('plays well with dotenv', () => {
   let dotEnvResult = dotEnv.config({ path: 'tests/test_files/dotenv.env' });
   requiredEnv.checkRequired({ environmentDefinition : 'tests/test_files/test_dotenv_required.env', checkInRequiredEnv : dotEnvResult.parsed });
 });
 
-tap.doesNotThrow('single checking that variable exists in requiredenv works', test => {
+tap.doesNotThrow('single checking that variable exists in requiredenv works', () => {
   process.env.TO_CHECK = "foo";
   requiredEnv.checkRequired({ environmentDefinition : 'tests/test_files/simple_check_works.env', checkInRequiredEnv : { 'TO_CHECK': 'unused value' }});
 });
